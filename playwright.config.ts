1ec39c80b11a6c752574687c6fb24fe506d4c820
harjotsinghgill <86789159+harjotsinghgill@users.noreply.github.com>
2025-02-18 15:32:50 +0530

    [PROD-2538]: update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,13 +4,7 @@ export default defineConfig({
   testDir: "./test/",
   /* Run tests in files in parallel */
   fullyParallel: false,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: process.env.CI ? 'github' : 'line',
   timeout: 60000, // Increase timeout to 60 seconds
 });
