d194d9cb5bce29726ed79f8355b293be1f818d7b
Sahil1109 <59727215+Sahil1109@users.noreply.github.com>
2025-06-11 13:43:03 +0530

    Add ProofOfAddressCredential schema

--- test/tools/generateCredentialsData.ts
+++ test/tools/generateCredentialsData.ts
@@ -27,6 +27,10 @@ const optimav1Schema = JSON.parse(
   fs.readFileSync('./schemas/json/OptimaV1Credential.json', 'utf-8')
 );
 
+const proofOfAddressSchema = JSON.parse(
+  fs.readFileSync('./schemas/json/ProofOfAddressCredential.json', 'utf-8')
+);
+
 const yyyymmdd = function (date: Date) {
   var mm = date.getMonth() + 1;
   var dd = date.getDate();
@@ -296,6 +300,53 @@ function generateOptimaV1Credential(callback?: (data: any) => void): any {
     publicAddress: faker.finance.ethereumAddress(),
   };
 
+  if (callback) {
+    callback(data);
+  }
+
+  return data;
+}
+
+function generateProofOfAddressCredential(callback?: (data: any) => void): any {
+  const data = jsf.generate(proofOfAddressSchema) as any;
+  const did = `did:receptor:redbelly:${faker.helpers.arrayElement([
+    'testnet',
+    'mainnet',
+  ])}:${faker.string.alphanumeric(42)}`;
+
+  data['@context'] =
+    'https://raw.githubusercontent.com/redbellynetwork/receptor-schema/refs/heads/main/schemas/json-ld/ProofOfAddressCredential.jsonld';
+  data.id = faker.string.uuid();
+  data.type = ['VerifiableCredential', 'ProofOfAddressCredential'];
+  data.issuanceDate = faker.date.past().toISOString();
+  data.expirationDate = faker.date.future().toISOString();
+  data.issuer = { id: faker.internet.url() };
+  data.credentialStatus = {
+    id: faker.internet.url(),
+    type: 'CredentialStatusList2021',
+    revocationNonce: faker.number.int(),
+  };
+  data.credentialSchema = {
+    id: 'https://raw.githubusercontent.com/redbellynetwork/receptor-schema/refs/heads/main/schemas/json/ProofOfAddressCredential.json',
+    type: 'JsonSchemaValidator2018',
+  };
+  data.subjectPosition = faker.helpers.arrayElement(['none', 'index', 'value']);
+  data.merklizationRootPosition = faker.helpers.arrayElement([
+    'none',
+    'index',
+    'value',
+  ]);
+  data.revNonce = faker.number.int();
+  data.version = faker.number.int();
+  data.updatable = faker.datatype.boolean();
+
+  data.credentialSubject = {
+    street: faker.location.street(),
+    city: faker.location.city(),
+    postCode: faker.location.zipCode('#####'),
+    country: faker.location.countryCode('alpha-3'),
+  };
+  console.log({ data: data.credentialSubject });
   if (callback) {
     callback(data);
   }
@@ -1568,6 +1619,84 @@ const optimaV1TestScenarios = [
   },
 ];
 
+const proofOfAddressTestScenarios = [
+  {
+    name: 'Valid ProofOfAddressCredential',
+    data: generateProofOfAddressCredential(),
+    expectedValid: true,
+  },
+  {
+    name: 'Missing Required Field: country',
+    data: generateProofOfAddressCredential((data) => {
+      delete data.credentialSubject.country;
+    }),
+    expectedValid: false,
+  },
+  {
+    name: 'Missing Required Field: city',
+    data: generateProofOfAddressCredential((data) => {
+      delete data.credentialSubject.city;
+    }),
+    expectedValid: false,
+  },
+  {
+    name: 'Missing Required Field: street',
+    data: generateProofOfAddressCredential((data) => {
+      delete data.credentialSubject.street;
+    }),
+    expectedValid: false,
+  },
+  {
+    name: 'Missing Required Field: postCode',
+    data: generateProofOfAddressCredential((data) => {
+      delete data.credentialSubject.postCode;
+    }),
+    expectedValid: false,
+  },
+  {
+    name: 'Wrong Data Type: postCode as number',
+    data: generateProofOfAddressCredential((data) => {
+      data.credentialSubject.postCode = 12345;
+    }),
+    expectedValid: false,
+  },
+  {
+    name: 'Wrong Data Type: city as number',
+    data: generateProofOfAddressCredential((data) => {
+      data.credentialSubject.city = 9876;
+    }),
+    expectedValid: false,
+  },
+  {
+    name: 'Malformed Context: undefined',
+    data: generateProofOfAddressCredential((data) => {
+      data['@context'] = undefined;
+    }),
+    expectedValid: false,
+  },
+  {
+    name: 'Invalid postCode: empty string',
+    data: generateProofOfAddressCredential((data) => {
+      data.credentialSubject.postCode = '';
+    }),
+    expectedValid: false,
+  },
+  {
+    name: 'Invalid postCode: alphanumeric',
+    data: generateProofOfAddressCredential((data) => {
+      data.credentialSubject.postCode = '123AB';
+    }),
+    expectedValid: false,
+  },
+  {
+    name: 'Invalid country: whitespace only',
+    data: generateProofOfAddressCredential((data) => {
+      data.credentialSubject.country = '   ';
+    }),
+    expectedValid: false,
+  },
+];
+
 const testObject = {
   AMLCTFCredential: amlCtfTestScenarios,
   DriversLicenceCredential: dLTestScenarios,
@@ -1575,6 +1704,7 @@ const testObject = {
   KYC1By1Credential: kyc1By1TestScenarios,
   PassportCredential: passportTestScenarios,
   OptimaV1Credential: optimaV1TestScenarios,
+  ProofOfAddressCredential: proofOfAddressTestScenarios,
 };
 
 if (!fs.existsSync('./test/data')) {
